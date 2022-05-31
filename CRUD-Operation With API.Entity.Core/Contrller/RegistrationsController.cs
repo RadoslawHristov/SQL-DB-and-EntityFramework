a528aa3337b516296a582ac05de9badec6f44262
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-05-31 22:04:49 +0300

    Add files via upload

---
+++ CRUD-Operation With API.Entity.Core/Contrller/RegistrationsController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DB_CRUD_Basic.Models;
+
+namespace DB_CRUD_Basic.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        private readonly SOFMAGContext _context;
+
+        public RegistrationsController(SOFMAGContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Registrations
+        public async Task<IActionResult> Index()
+        {
+              return _context.Registrations != null ? 
+                          View(await _context.Registrations.ToListAsync()) :
+                          Problem("Entity set 'SOFMAGContext.Registrations'  is null.");
+        }
+
+        // GET: Registrations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Registrations == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registrations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        // GET: Registrations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Data")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(registration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Registrations == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registrations.FindAsync(id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Data")] Registration registration)
+        {
+            if (id != registration.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(registration);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegistrationExists(registration.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Registrations == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registrations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        // POST: Registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Registrations == null)
+            {
+                return Problem("Entity set 'SOFMAGContext.Registrations'  is null.");
+            }
+            var registration = await _context.Registrations.FindAsync(id);
+            if (registration != null)
+            {
+                _context.Registrations.Remove(registration);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegistrationExists(int id)
+        {
+          return (_context.Registrations?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
