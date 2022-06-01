ebc1e63a303dffeb0ac15dc0e5337f637a0c8e32
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-06-01 20:32:44 +0300

    Add files via upload

---
+++ CRUD-Operation With API.Entity.Core/Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace DB_CRUD_Basic.Models
+{
+    public partial class Registration
+    {
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; } = null!;
+        [DisplayName("Last Name")]
+        public string LastName { get; set; } = null!;
+        [DisplayName("Date Of Birth")]
+        public DateTime? Data { get; set; }
+    }
+}
