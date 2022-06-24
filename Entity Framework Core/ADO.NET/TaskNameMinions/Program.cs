ccf2166f74398ee882ab310fbb3cbc0f7205b3d7
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-06-24 13:14:47 +0300

    Add files via upload

---
+++ Entity Framework Core/ADO.NET/TaskNameMinions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+
+namespace Villain_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connect = new SqlConnection(Config.ConnectionStr);
+
+
+            connect.Open();
+
+            string Countquery = @" SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount
+                                    FROM Villains AS v
+                                    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                    GROUP BY v.Id, v.Name
+                                        HAVING COUNT(mv.VillainId) > 3
+                                    ORDER BY COUNT(mv.VillainId)";
+
+
+            SqlCommand countOfMinions = new SqlCommand(Countquery,connect);
+
+            SqlDataReader reader = countOfMinions.ExecuteReader();
+
+            while (reader.Read())
+            {
+                    
+                    string name = (string)reader["Name"];
+                    int idEvil = (int)reader["MinionsCount"];
+
+                    Console.WriteLine($" {name} - {idEvil}");
+            }
+            connect.Close();
+        }
+    }
+}
