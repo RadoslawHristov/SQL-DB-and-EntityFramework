c8022acba246faead0189e2c06a0838b64ffa065
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-06-27 20:00:55 +0300

    Add files via upload

---
+++ Entity Framework Core/ADO.NET/IncreaseAgeForProcedure/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Increase_Age_Stored_Procedure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthdayMinionId = int.Parse(Console.ReadLine());
+
+            var connection = new SqlConnection(Config.KeyStr);
+            connection.Open();
+
+            using (connection)
+            {
+                string cmdText = "EXEC usp_GetOlder @id";
+                var command = new SqlCommand(cmdText, connection);
+                command.Parameters.AddWithValue("@id", birthdayMinionId);
+                command.ExecuteNonQuery();
+
+                string printQuery = "SELECT Name, Age FROM Minions WHERE Id = @id";
+                var printCommand = new SqlCommand(printQuery, connection);
+                printCommand.Parameters.AddWithValue("id", birthdayMinionId);
+                var printer = printCommand.ExecuteReader();
+
+                printer.Read();
+                string minionName = (string)printer["Name"];
+                int age = (int)printer["Age"];
+                printer.Close();
+
+                Console.WriteLine($"{minionName} - {age} years old");
+            }
+        }
+    }
+}
