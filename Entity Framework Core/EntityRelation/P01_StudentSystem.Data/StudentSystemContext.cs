6a2088ff3cde3e55b8c40bf2b4e1c20de6df588c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-07-11 12:23:51 +0300

    Add files via upload

---
+++ Entity Framework Core/EntityRelation/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions option)
+        : base(option)
+        {
+
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Resource> Resources { get; set; }
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.Connect);
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(e =>
+            {
+                e.HasKey(sc => new { sc.StudentId, sc.CourseId });
+            });
+        }
+    }
+}
