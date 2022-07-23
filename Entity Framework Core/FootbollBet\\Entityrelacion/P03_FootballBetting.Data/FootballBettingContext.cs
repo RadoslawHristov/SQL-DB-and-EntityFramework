6db1705733f37c491d4e43d519fb79a3cca9dbe0
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-07-23 18:34:24 +0300

    Add files via upload

---
+++ Entity Framework Core/FootbollBet\\Entityrelacion/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting.Data
+{
+    public class FootballBettingContext : DbContext
+    {
+
+        public FootballBettingContext()
+        {
+            
+        }
+
+        public FootballBettingContext(DbContextOptions options)
+        :base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Bet> Bets { get; set; }
+        public virtual DbSet<Color> Colors { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Game> Games { get; set; }
+        public virtual DbSet<Player> Players { get; set; }
+        public virtual DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+        public virtual DbSet<Team> Teams{ get; set; }
+        public virtual DbSet<Town> Towns{ get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.Connection);
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerStatistic>()
+                .HasKey(ps => new {ps.GameId,ps.PlayerId });
+
+        }
+    }
+}
