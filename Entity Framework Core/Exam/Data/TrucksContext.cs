7409ef547f8b31f7c08e3bfa94489c5ad1df1bdc
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-08-18 11:51:41 +0300

    Add files via upload

---
+++ Entity Framework Core/Exam/Data/TrucksContext.cs
@@ -0,0 +1,39 @@
+﻿using Trucks.Data.Models;
+
+namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext() { }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Truck> Trucks { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+
+        public virtual DbSet<Despatcher> Despatchers { get; set; }
+
+        public virtual DbSet<ClientTruck> ClientsTrucks { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClientTruck>().HasKey(c => new { c.ClientId, c.TruckId });
+        }
+    }
+}
