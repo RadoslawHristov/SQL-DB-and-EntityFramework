cf3b3b31365c45fc8a96183b33dae7883797a323
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-08-21 17:55:44 +0300

    Add files via upload

---
+++ Entity Framework Core/Exam/DataProcesor/ExportDTO/ExportDespecherDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Despatcher")]
+    public class ExportDespecherDto
+    {     
+        [XmlAttribute("TrucksCount")]
+        public int TrucksCount { get; set; }
+
+        [XmlElement("DespatcherName")]
+        public string Name { get; set; }
+
+
+        [XmlArray("Trucks")]
+        public ExportDespTruckDto[] Trucks { get; set; }
+    }
+}
