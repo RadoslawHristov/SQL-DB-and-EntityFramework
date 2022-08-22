7e8cf917b6fcbecbb12f88c92cc81dc7cdba1cff
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-08-22 20:04:24 +0300

    Add files via upload

---
+++ Entity Framework Core/Exam/DataProcesor/Deserializer.cs
@@ -0,0 +1,167 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Trucks.Data.Models;
+using Trucks.Data.Models.Enums;
+using Trucks.DataProcessor.ImportDto;
+
+namespace Trucks.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Data;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedDespatcher
+            = "Successfully imported despatcher - {0} with {1} trucks.";
+
+        private const string SuccessfullyImportedClient
+            = "Successfully imported client - {0} with {1} trucks.";
+
+        public static string ImportDespatcher(TrucksContext context, string xmlString)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlRootAttribute root = new XmlRootAttribute("Despatchers");
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportDespacheDto[]),root);
+            ImportDespacheDto[] despacheDtos;
+
+            ICollection<Despatcher> despatchers = new List<Despatcher>();
+
+            using ( StringReader reader = new StringReader(xmlString))
+            {
+                despacheDtos = (ImportDespacheDto[])serializer.Deserialize(reader);
+            }
+
+            foreach (var dto in despacheDtos)
+            {
+                if (!IsValid(dto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(dto.Position))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Despatcher despatcher = new Despatcher()
+                {
+                    Name = dto.Name,
+                    Position = dto.Position
+                };
+
+                foreach (var tru in dto.Trucks)
+                {
+                    if (!IsValid(tru))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    bool isValidcategory = Enum.TryParse(typeof(CategoryType),tru.CategoryType,out object caResult );
+                    if (!isValidcategory)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    bool isValidType = Enum.TryParse(typeof(MakeType), tru.MakeType, out object tResult);
+                    if (tru.CargoCapacity==null || tru.TankCapacity == null) 
+                    {
+
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    Truck truck = new Truck()
+                    {
+                        RegistrationNumber = tru.RegistrationNumber,
+                        VinNumber = tru.VinNumber,
+                        TankCapacity = tru.TankCapacity,
+                        CargoCapacity = tru.CargoCapacity,
+                        CategoryType = (CategoryType)caResult,
+                        MakeType = (MakeType)tResult
+                    };
+
+                    despatcher.Trucks.Add(truck);
+                   
+                }
+                despatchers.Add(despatcher);
+                sb.AppendLine(String.Format(SuccessfullyImportedDespatcher,despatcher.Name,despatcher.Trucks.Count));
+            }
+
+           context.AddRange(despatchers);
+           context.SaveChanges();
+
+
+            return sb.ToString().TrimEnd();
+        }
+        public static string ImportClient(TrucksContext context, string jsonString)
+        {
+            ImportClietDto[] importClietDtos = JsonConvert.DeserializeObject<ImportClietDto[]>(jsonString);
+            StringBuilder sb = new StringBuilder();
+
+            ICollection<ClientTruck> clientTrucks = new List<ClientTruck>();
+            ICollection<Client> clients = new List<Client>();
+            foreach (var dto in importClietDtos)
+            {
+                if (!IsValid(dto) || dto.Type== "usual")
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Client client = new Client()
+                {
+                    Name = dto.Name,
+                    Nationality = dto.Nationality,
+                    Type = dto.Type
+                };
+
+                clients.Add(client);
+                foreach (var truck in dto.Trucks.Distinct())
+                {
+                    var trucks = context.Trucks.FirstOrDefault(x => x.Id == truck);
+
+                    if (trucks==null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+
+                    ClientTruck clientTruck = new ClientTruck()
+                    {
+                        Client = client,
+                        TruckId = truck
+                    };
+                    client.ClientsTrucks.Add(clientTruck);
+                    clientTrucks.Add(clientTruck);
+                }
+
+                sb.AppendLine(String.Format(SuccessfullyImportedClient,client.Name,client.ClientsTrucks.Count));
+
+            }
+
+            context.AddRange(clientTrucks);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Entity Framework Core/Exam/DataProcesor/Serializer.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Trucks.DataProcessor.ExportDto;
+
+namespace Trucks.DataProcessor
+{
+    using System;
+    using Data;
+
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            ExportDespecherDto[] exportDespecherDtos;
+
+            var results = context.Despatchers
+                .ToArray()
+                .Where(x => x.Trucks.Count > 0)
+                .Select(a => new ExportDespecherDto()
+                {
+                    TrucksCount = a.Trucks.Count,
+                    Name = a.Name,
+                    Trucks = a.Trucks.Select(x => new ExportDespTruckDto()
+                    {
+                        RegistrationNumber = x.RegistrationNumber,
+                        Make = x.MakeType.ToString()
+                    })
+                        .OrderBy(x => x.RegistrationNumber)
+                        .ToArray()
+                })
+                .OrderByDescending(z => z.TrucksCount)
+                .ThenBy(x => x.Name)
+                .ToArray();
+            XmlRootAttribute root = new XmlRootAttribute("Despatchers");
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportDespecherDto[]), root);
+            var namsp = new XmlSerializerNamespaces();
+            namsp.Add("", "");
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, results, namsp);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
+        {
+
+            var results = context.Clients
+                .ToList()
+                .Where(c => c.ClientsTrucks.Count > 0)
+                .Select(a => new
+                {
+                    Name = a.Name,
+                    Trucks = a.ClientsTrucks
+                        .Select(t => new
+                        {
+                            TruckRegistrationNumber = t.Truck.RegistrationNumber,
+                            VinNumber = t.Truck.VinNumber,
+                            TankCapacity = t.Truck.TankCapacity,
+                            CargoCapacity = t.Truck.CargoCapacity,
+                            CategoryType = t.Truck.CategoryType.ToString(),
+                            MakeType = t.Truck.MakeType.ToString()
+                        })
+                        .Where(x => x.TankCapacity >= capacity)
+                        .OrderBy(z => z.MakeType)
+                        .ThenByDescending(s => s.CargoCapacity)
+                        .ToList()
+                })
+                .OrderByDescending(d => d.Trucks.Count)
+                .ThenBy(n => n.Name)
+                .Take(10)
+                .ToList();
+
+
+
+            var json = JsonConvert.SerializeObject(results, Formatting.Indented);
+
+
+            return json;
+        }
+    }
+}
