3eabacdd637a386d830ffd9033ebfbbbe738a18f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-08-25 20:15:46 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamsTraning/Exam-Theathre/Data/Model/Cast.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Theatre.Data.Models
+{
+    public class Cast
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30,MinimumLength = 4)]
+        public string FullName { get; set; }
+
+        [Required]
+        public bool IsMainCharacter { get; set; }
+
+        [Required] 
+        [StringLength(15)]
+        [RegularExpression(@"^\+44\-\d{2}\-\d{3}\-\d{4}$")]
+        public string PhoneNumber { get; set; }
+
+
+        [ForeignKey(nameof(Play))]
+        public int PlayId { get; set; }
+        public Play Play { get; set; }
+    }
+}
---
+++ Entity Framework Core/ExamsTraning/Exam-Theathre/Data/Model/Play.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        public Play()
+        {
+            this.Casts = new List<Cast>();
+            this.Tickets = new List<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50,MinimumLength = 4)]
+        public string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "time")]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        [Range(typeof(float),"0.0","10.0")]
+        public float Rating { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [MaxLength(700)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(30,MinimumLength = 4)]
+        public string Screenwriter { get; set; }
+
+        public List<Cast> Casts { get; set; }
+
+        public List<Ticket> Tickets { get; set; }
+    }
+}
