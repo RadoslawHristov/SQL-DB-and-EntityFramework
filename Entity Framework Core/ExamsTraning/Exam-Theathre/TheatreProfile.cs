ef6d3072a0e26d621941c153055873bd8e913408
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-09-07 12:39:13 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamsTraning/Exam-Theathre/TheatreProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace Theatre
+{
+    using AutoMapper;
+    using System.Globalization;
+    using Theatre.Data.Models;
+    using Theatre.DataProcessor.ExportDto;
+
+    class TheatreProfile : Profile
+    {
+        // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+        public TheatreProfile()
+        {
+            CreateMap<Cast, ExportCastDto>();
+            CreateMap<Play, ExportPlayerDto>()
+                .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration.ToString("c", CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.ToString()))
+                .ForMember(dest => dest.Actors, opt => opt.MapFrom(src => src.Casts.Where(c => c.IsMainCharacter).ToList()))
+                .ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.Rating == 0 ? "Premier" : src.Rating.ToString()));
+        }
+    }
+}
