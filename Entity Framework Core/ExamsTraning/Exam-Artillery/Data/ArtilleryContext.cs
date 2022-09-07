ddac99c6b5fe04b75810a6e4c07a673d4331e620
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-09-07 12:43:18 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamsTraning/Exam-Artillery/Data/ArtilleryContext.cs
@@ -0,0 +1,43 @@
+﻿using System.IO.Compression;
+using System.Xml.Serialization;
+using Artillery.Data.Models;
+
+namespace Artillery.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ArtilleryContext : DbContext
+    {
+        public ArtilleryContext() { }
+
+        public ArtilleryContext(DbContextOptions options)
+            : base(options) { }
+
+
+        public virtual DbSet<Country> Countries { get; set; }
+
+        public virtual DbSet<Manufacturer>  Manufacturers { get; set; }
+
+        public virtual DbSet<Shell> Shells { get; set; }    
+
+        public virtual DbSet<Gun>  Guns { get; set; }
+
+        public virtual DbSet<CountryGun> CountriesGuns { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CountryGun>().HasKey(x => new {x.CountryId,x.GunId });
+
+            modelBuilder.Entity<Manufacturer>().HasIndex(u => u.ManufacturerName).IsUnique();
+        }
+    }
+}
