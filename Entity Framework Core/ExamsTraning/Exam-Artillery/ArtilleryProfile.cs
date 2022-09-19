88114d65e3908a2767e8cb5913d2195726cf1f09
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-09-19 12:35:13 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamsTraning/Exam-Artillery/ArtilleryProfile.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Artillery
+{
+    using Artillery.Data.Models;
+    using Artillery.DataProcessor.ExportDto;
+    using Artillery.DataProcessor.ImportDto;
+    using AutoMapper;
+    class ArtilleryProfile : Profile
+    {
+        // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+        public ArtilleryProfile()
+        {
+            CreateMap<Country, ExportManfucWithGunsDto>();
+            CreateMap<Gun, ExportGunsDto>()
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.ManufacturerName))
+                .ForMember(dest => dest.GunType, opt => opt.MapFrom(src => src.GunType.ToString()))
+                .ForMember(dest => dest.Countries, opt => opt.MapFrom(src => src.CountriesGuns.Select(cg => cg.Country)));
+        }
+    }
+}
